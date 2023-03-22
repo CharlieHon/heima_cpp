57dfdbdd91e30db746e6c9a0aebd0c242953614e
charlie <2295509496@qq.com>
2023-03-22 19:00:13 +0800

    cpp基础

---
+++ 01_sizeof.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "short 类型所占内存空间为：" << sizeof(short) << endl;
+	cout << "int 类型所占内存空间为：" << sizeof(int) << endl;
+	cout << "long 类型所占内存空间为：" << sizeof(long) << endl;
+	cout << "long long 类型所占内存空间为：" << sizeof(long long) << endl;
+	return 0;
+}
