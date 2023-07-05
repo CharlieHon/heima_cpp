e4671026aed92150cf7993a96e6af1ac14041bed
charlie <2295509496@qq.com>
2023-07-05 11:07:56 +0800

    '结构体案例'

--- code/08_struct/06_struct.cpp
+++ code/08_struct/06_struct.cpp
@@ -1,56 +1,59 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <cstdlib>
 using namespace std;
 
+// 学生的结构体
 struct Student{
+    // 姓名
     string name;
+    // 分数
     int score;
 };
 
+// 老师的结构体定义
 struct Teacher{
-    string name;
-    struct Student stuArray[5]; //每名老师带5和学生
+    // 姓名
+    string tname;
+    // 学生数组
+    Student sArray[5];
 };
 
-//给老师和学生赋值的函数
-void allocateSpace(Teacher *t, const int n){
-    string names = "ABCDEFG";
-    for(int i=0; i<n; ++i){
-        (t+i)->name = string("Teacher_") + names[i];
-        //给老师带的学生赋值
+// 给老师和学生信息赋值
+void allocateSpace(Teacher tArray[], int len){
+    string nameSeed = "ABCDE";
+    // 给老师开始赋值
+    for(int i=0; i<len; ++i){
+        tArray[i].tname = string("Teacher_") + nameSeed[i];
+        // 通过循环给每名老师所带的学生赋值
         for(int j=0; j<5; ++j){
-            (t+i)->stuArray[j].name = string("Stu_") + names[j];
-            int random = rand() % 61 + 40;  // 0~60 + 40
-            (t+i)->stuArray[j].score = random;
+            tArray[i].sArray[j].name = string("Student_") + nameSeed[j];
+            tArray[i].sArray[j].score = rand()%61+40;   // [0, 100]
         }
     }
 }
 
-//打印所有信息
-void printInfo(const Teacher *t, const int n){
-    for(int i=0; i<n; ++i){
-        cout << "Teacher_name: " << (t+i)->name << endl;
+// 打印所有信息
+void printInfo(struct Teacher tArray[], int len){
+    for(int i=0; i<len; ++i){
+        cout << tArray[i].tname << endl;
         for(int j=0; j<5; ++j){
-            cout << "Student_name: " << (t+i)->stuArray[j].name
-                << " Student_score: " << (t+i)->stuArray[j].score
-                << endl;
+            cout << "\t" << tArray[i].sArray[j].name << ": " << tArray[i].sArray[j].score << endl;
         }
     }
 }
 
 int main()
 {
-    //随机数种子
-    srand((unsigned int)time(NULL));
-
-    //1.创建3名老师的数组
-    Teacher t[3];
-    const int n = sizeof(t)/sizeof(t[0]);
-    //2.通过函数给3名老师的信息赋值，并给老师带的学生信息赋值
-    allocateSpace(t, n);
-    //3.打印所有老师及其学生信息
-    printInfo(t, n);
+    srand(time(NULL));  // 随机种子初始化
+    // 1.创建3名老师的数组
+    struct Teacher tArray[3];
+    int len = sizeof(tArray)/sizeof(tArray[0]);
+    // 2.通过函数给3名老师信息复制，并给老师带的学生信息赋值
+    allocateSpace(tArray, len);
+    // 3.打印所有老师及所带的学生信息
+    printInfo(tArray, len);
 
     return 0;
 }
